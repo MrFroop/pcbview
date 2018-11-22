4ea514515bc1edcdc6d08cf3ef384d88a18fc36d
Fredrik Jambrén <fredrik@jambren.com>
2018-11-22 22:58:08 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+	kotlin("jvm") version "1.3.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val junitVersion = "5.3.1"
+val tornadofxVersion = "1.7.17"
+
+application {
+	mainClassName = "com.jambren.pcbview.app.PcbView"
+}
+
+val compileKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
+
+dependencies {
+    compile(kotlin("stdlib"))
+
+    compile(group = "no.tornado", name = "tornadofx", version = tornadofxVersion)
+
+    testCompile(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
+    testRuntime(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
+}
