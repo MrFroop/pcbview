853c114e91d915166cc62cafdcf076cd2a36f29e
Fredrik Jambrén <fredrik@jambren.com>
2018-11-23 19:39:25 +0100

    Fixed code style and added editor config.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-	kotlin("jvm") version "1.3.10"
+    kotlin("jvm") version "1.3.10"
     id("org.sonarqube") version "2.6.2"
     id("io.gitlab.arturbosch.detekt") version "1.0.0-RC11"
 }
@@ -13,10 +13,10 @@ repositories {
 }
 
 val junitVersion = "5.3.1"
-val tornadofxVersion = "1.7.17"
+val tornadoFXVersion = "1.7.17"
 
 application {
-	mainClassName = "com.jambren.pcbview.app.PcbView"
+    mainClassName = "com.jambren.pcbview.app.PcbView"
 }
 
 val compileKotlin: KotlinCompile by tasks
@@ -24,9 +24,10 @@ val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions.jvmTarget = "1.8"
 
 dependencies {
-    compile(kotlin("stdlib"))
+    compile(kotlin("stdlib-jdk8"))
+    compile(kotlin("reflect"))
 
-    compile(group = "no.tornado", name = "tornadofx", version = tornadofxVersion)
+    compile(group = "no.tornado", name = "tornadofx", version = tornadoFXVersion)
 
     testCompile(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
     testRuntime(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
